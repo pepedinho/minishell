bb9857ce59a9463e3a9ee6d8d80387c4f9b17d43
pepedinho <2spii94@gmail.com>
2024-07-31 05:11:49 +0200

    reproduct bash behaviour with heredoc fail error

--- srcs/execution/exec.c
+++ srcs/execution/exec.c
@@ -77,30 +77,41 @@ void	file(t_element *tmp)
 int	open_file(t_command_line *queue)
 {
 	t_element	*tmp;
+	int			i;
+	int			j;
 
-	tmp = queue->first;
-	while (tmp)
-	{
-		if (tmp->type == H_FILE)
-			here_doc(tmp);
-		tmp = tmp->next;
-	}
+	i = 0;
+	j = 0;
 	tmp = queue->first;
 	if (queue->u_token_flag == 1)
 	{
 		while (tmp && tmp->type != U_TOKEN)
-			tmp = tmp->next;
-		handle_unexpected_token(tmp->content);
-		return (0);
-	}
-	else
-	{
-		while (tmp)
 		{
-			if (tmp->type == FILE)
-				file(tmp);
 			tmp = tmp->next;
+			i++;
 		}
+		if (tmp)
+			handle_unexpected_token(tmp->content);
+	}
+	tmp = queue->first;
+	while (tmp)
+	{
+		if (tmp->type == H_FILE)
+		{
+			if (j < i)
+				here_doc(tmp);
+			else
+				return (0);
+		}
+		tmp = tmp->next;
+		j++;
+	}
+	tmp = queue->first;
+	while (tmp)
+	{
+		if (tmp->type == FILE)
+			file(tmp);
+		tmp = tmp->next;
 	}
 	return (1);
 }
