3f91ac7fdf8bc7e2f6b0922d7fb06d4905ec27bf
OppaiFun <moussaadamou898@gmail.com>
2024-08-03 15:23:30 +0200

    change cd function, now wwe have same error output like bash

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: madamou <madamou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 16:11:50 by madamou           #+#    #+#             */
-/*   Updated: 2024/07/29 16:12:14 by madamou          ###   ########.fr       */
+/*   Updated: 2024/08/03 15:22:40 by madamou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,14 @@
 
 int	ft_cd(char *directory)
 {
-	return (chdir(directory));
+	t_info	*info;
+
+	info = info_in_static(NULL, GET);
+	g_signal_code = 0;
+	if (chdir(directory) == -1)
+	{
+		perror(ft_sprintf("%s: %s", info->name, directory));
+		g_signal_code = 1;
+	}
+	return (g_signal_code);
 }
