d5bd01004f5b8fae8a6ec3600074ad9ea654d6bb
OppaiFun <moussaadamou898@gmail.com>
2024-08-03 15:26:10 +0200

    fix cd

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: madamou <madamou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 16:11:50 by madamou           #+#    #+#             */
-/*   Updated: 2024/08/03 15:22:40 by madamou          ###   ########.fr       */
+/*   Updated: 2024/08/03 15:25:49 by madamou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,17 @@
 int	ft_cd(char *directory)
 {
 	t_info	*info;
+	char	*message;
 
 	info = info_in_static(NULL, GET);
 	g_signal_code = 0;
 	if (chdir(directory) == -1)
 	{
-		perror(ft_sprintf("%s: %s", info->name, directory));
+		message = ft_sprintf("%s: %s", info->name, directory);
+		if (!message)
+			handle_malloc_error("cd");
+		perror(message);
+		free(message);
 		g_signal_code = 1;
 	}
 	return (g_signal_code);
