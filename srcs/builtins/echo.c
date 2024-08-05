06ed9a077acce920c62af5ec062fe0f76cb612ac
JudaNanaa <moussaadamou898@gmail.com>
2024-08-06 01:45:14 +0200

    echo change for no segfault

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: madamou <madamou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 16:01:22 by madamou           #+#    #+#             */
-/*   Updated: 2024/08/05 20:31:44 by madamou          ###   ########.fr       */
+/*   Updated: 2024/08/06 01:44:35 by madamou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,33 @@ void	ft_echo(char **args, t_info *info)
 {
 	int	i;
 
-	if (ft_strcmp(args[1], "-n") == 0)
+	if (args[1])
 	{
-		i = 2;
-		while (args[i])
+		if (ft_strcmp(args[1], "-n") == 0)
 		{
-			ft_printf("%s", args[i]);
-			if (args[i + 1])
-				ft_printf(" ");
-			i++;
+			i = 2;
+			while (args[i])
+			{
+				ft_printf("%s", args[i]);
+				if (args[i + 1])
+					ft_printf(" ");
+				i++;
+			}
 		}
-	}
-	else if (ft_strcmp(args[1], "?") == 0)
-		printf("%d\n", info->signal_code);
-	else
-	{
-		i = 1;
-		while (args[i])
+		else if (ft_strcmp(args[1], "?") == 0)
+			printf("%d\n", info->signal_code);
+		else
 		{
-			ft_printf("%s", args[i]);
-			if (args[i + 1])
-				ft_printf(" ");
-			i++;
+			i = 1;
+			while (args[i])
+			{
+				ft_printf("%s", args[i]);
+				if (args[i + 1])
+					ft_printf(" ");
+				i++;
+			}
+			ft_printf("\n");
 		}
-		ft_printf("\n");
 	}
 	info->signal_code = 0;
 }
