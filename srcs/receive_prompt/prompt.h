ee2e010d63ea436f4bfe6d012c8bea9c6038b18f
OppaiFun <moussaadamou898@gmail.com>
2024-08-10 19:49:37 +0200

    commit for push

--- srcs/receive_prompt/prompt.h
+++ srcs/receive_prompt/prompt.h
@@ -6,7 +6,7 @@
 /*   By: madamou <madamou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 19:02:19 by madamou           #+#    #+#             */
-/*   Updated: 2024/08/04 21:33:46 by madamou          ###   ########.fr       */
+/*   Updated: 2024/08/10 17:48:52 by madamou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,7 @@ void				receive_prompt(t_info *info);
 void	receive_prompt_subminishell(char *command_line,
 									t_info *info);
 t_info				*info_in_static(t_info *info, int cas);
+char	*ft_readline(t_info *info);
+
 
 #endif
