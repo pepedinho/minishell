77f5e8a77cf1eba82a33d9adbbf59969ae20342a
pepedinho <2spii94@gmail.com>
2024-08-21 18:37:50 +0200

    have to fix some issues

--- srcs/wildcards/wildcards.c
+++ srcs/wildcards/wildcards.c
@@ -62,25 +62,26 @@ char	*get_new_path(char *path, int depth, char *dirname)
 {
 	char	*result;
 	char	*first_elem;
-	int		depth_cnt;
+	int		dpth;
 	int		i;
 
-	(void)depth;
-	(void)depth_cnt;
+	dpth = 0;
 	i = 0;
 	while (path[i])
 	{
-		if (path[i] == '/')
+		if (path[i] == '/' && dpth == depth - 1)
 			break ;
+		if (path[i] == '/')
+			dpth++;
 		i++;
 	}
-	// printf("[[1][DEBUG]] path : %s\n", path);
+	printf("[[1][DEBUG]] path : %s\n", path);
 	first_elem = ft_substr(path, 0, i);
 	if (first_elem[ft_strlen(first_elem) - 1] != '/')
 		result = ft_sprintf("%s/%s", first_elem, dirname);
 	else
 		result = ft_strjoin(first_elem, dirname);
-	// printf("[[2][DEBUG]] result : %s\n", result);
+	printf("[[2][DEBUG]] result : %s\n", result);
 	return (result);
 }
 
@@ -120,23 +121,23 @@ void	rec_open(char **tab, int depth, int cnt, t_command_line *queue)
 	elem = readdir(dir);
 	while (elem)
 	{
-		if (elem->d_type == 4 && !ft_strstr(elem->d_name, ".") && depth
+		if (elem->d_type == 4 && !ft_strstr(elem->d_name, ".") && depth > 0
 			&& cnt == tot_cnt)
 		{
 			// printf("debug[1] : %s\n", ft_strstr(elem->d_name, "."));
 			// new_dir = ft_strjoin(tab[DIRNAME], elem->d_name);
 			if (!compare(tab[DIRNAME], elem->d_name))
 			{
-				printf("list : %s/%s\n\tdepth : %d\n", tab[DIRNAME],
-					elem->d_name, depth);
-				new_dir = get_new_path(tab[DIRNAME], 0, elem->d_name);
+				printf("list : %s/\n\tdepth : %d\n", tab[DIRNAME], depth);
+				new_dir = get_new_path(tab[DIRNAME], depth, elem->d_name);
 				list_file(new_dir, tab[PATERN], queue);
 				tab[DIRNAME] = new_dir;
 				// printf("debug[2] : %s\n", elem->d_name);
 				rec_open(tab, depth - 1, 0, queue);
 			}
 		}
-		else if (elem->d_type == 4 && !ft_strstr(elem->d_name, ".") && depth)
+		else if (elem->d_type == 4 && !ft_strstr(elem->d_name, ".")
+			&& depth > 0)
 		{
 			// list_file(tab[DIRNAME], tab[PATERN], queue);
 			// printf("debug[1] : %s\n", ft_strstr(elem->d_name, "."));
@@ -144,7 +145,7 @@ void	rec_open(char **tab, int depth, int cnt, t_command_line *queue)
 			if (!compare(tab[DIRNAME], elem->d_name))
 			{
 				printf("debug : %s\n\tdepth : %d\n", tab[DIRNAME], depth);
-				new_dir = get_new_path(tab[DIRNAME], 0, elem->d_name);
+				new_dir = get_new_path(tab[DIRNAME], depth, elem->d_name);
 				tab[DIRNAME] = new_dir;
 				rec_open(tab, depth - 1, 0, queue);
 			}
@@ -217,11 +218,72 @@ char	**get_path_and_format(char *str)
 	return (tab);
 }
 
+int	rec_depth_check(char **tab, int depth, int cnt)
+{
+	DIR				*dir;
+	struct dirent	*elem;
+	char			*new_dir;
+
+	if (cnt >= depth)
+		return (cnt);
+	dir = opendir(tab[DIRNAME]);
+	if (!dir)
+		return (0);
+	elem = readdir(dir);
+	while (elem)
+	{
+		if (elem->d_name[0] != '.' && elem->d_type == 4)
+		{
+			new_dir = get_new_path(tab[DIRNAME], 0, elem->d_name);
+			tab[DIRNAME] = new_dir;
+			return (rec_depth_check(tab, depth - 1, cnt + 1));
+		}
+		elem = readdir(dir);
+	}
+	return (cnt);
+}
+
+int	check_depth(char **tab, int depth, int cnt)
+{
+	DIR				*dir;
+	struct dirent	*elem;
+	char			*new_dir;
+	int				current_depth;
+	int				last_max_depth;
+
+	if (cnt >= depth)
+		return (cnt);
+	dir = opendir(tab[DIRNAME]);
+	if (!dir)
+		return (0);
+	last_max_depth = 0;
+	elem = readdir(dir);
+	while (elem)
+	{
+		if (elem->d_name[0] && elem->d_type == 4)
+		{
+			new_dir = get_new_path(tab[DIRNAME], 0, elem->d_name);
+			tab[DIRNAME] = new_dir;
+			current_depth = rec_depth_check(tab, depth, 0);
+			printf("debug : cur_depth : %d cur_file : %s\n", current_depth,
+				new_dir);
+			if (last_max_depth < current_depth)
+				last_max_depth = current_depth;
+		}
+		elem = readdir(dir);
+	}
+	return (last_max_depth + 1);
+}
+
 int	expend_wcards(char *path, t_command_line *queue)
 {
 	char	**tab;
+	int		max_depth;
 
 	tab = get_path_and_format(path);
+	max_depth = check_depth(tab, get_depth(path), 0);
+	tab = get_path_and_format(path);
+	printf("\nmax depth found : %d depth : %d\n\n", max_depth, get_depth(path));
 	rec_open(tab, get_depth(path) - 1, 0, queue);
 	return (0);
 }
